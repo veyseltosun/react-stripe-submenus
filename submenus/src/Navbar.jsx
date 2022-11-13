266eb60bca70ea34471c97db97a9a6a5e490e9db
veyseltosun <postalarim1923@gmail.com>
2022-11-14 02:26:22 +0300

    side bar.

--- submenus/src/Navbar.jsx
+++ submenus/src/Navbar.jsx
@@ -14,6 +14,18 @@ function Navbar() {
 
                 </button>
             </div>
+            <ul className='nav-links'>
+              <li>
+                <button className='link-btn' >products</button>
+              </li>
+              <li>
+                <button className='link-btn' >developers</button>
+              </li>
+              <li>
+                <button className='link-btn' >company</button>
+              </li>
+            </ul>
+            <button className='btn signin-btn'>Sign in</button>
         </div>
     </nav>
   )
--- submenus/src/Sidebar.jsx
+++ submenus/src/Sidebar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import {FaTimes} from "react-icons/fa";
+import sublinks from "./Data";
+
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar-wrapper show'>
+     <aside className='sidebar'>
+      <button className='close-btn'>
+        <FaTimes></FaTimes>
+      </button>
+      <div className='sidebar-links'>
+        {sublinks.map((item,index)=>{
+           const{page, links} = item;
+           return(
+             <article key={index}>
+               <h4 >{page}</h4>
+               <div className='sidebar-sublinks'>
+                 {links.map((link, index)=>{
+                   const{url, icon, label} = link;
+                   return(
+                     <a key={index} href={url}>
+                       {icon}
+                       {label}
+                     </a>
+                   )
+                 })}
+               </div>
+             </article>
+        )})}
+      </div>
+     </aside>
+
+    </div>
   )
 }
 
